54978f7fee6467e285616dbfa7cdf5833dc7f0db
Kidduu2123 <pavani5696@gmail.com>
2025-07-29 12:18:43 +0530

    Create looping.tf

---
+++ looping.tf
@@ -0,0 +1,16 @@
+variable filename{
+type = list(string)
+default = ["a1.txt","b1.txt","c1.txt"]
+}
+
+#resource "local_file" "f4"{
+#count = length(var.filename)
+#filename = var.filename[count.index]
+#content = "test"
+#}
+
+resource "local_file""f9"{
+filename = each.value
+for_each = toset(var.filename)
+content = "test"
+}
